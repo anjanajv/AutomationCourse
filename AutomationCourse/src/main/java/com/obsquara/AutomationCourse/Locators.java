87a65da489c0701ad890fe3e2413c4e5c32f6198
anjan <anjan@LAPTOP-2S8BC3KU>
2023-05-17 18:43:42 +0530

    locators-css,linktext,partialtext

--- AutomationCourse/src/main/java/com/obsquara/AutomationCourse/Locators.java
+++ AutomationCourse/src/main/java/com/obsquara/AutomationCourse/Locators.java
@@ -38,12 +38,64 @@ public class Locators extends Base{
 	driver.findElement(By.name("q"));
 	driver.navigate().back();
 	}
+	public void locateByCssSelector() {
+	driver.findElement(By.cssSelector("input#twotabsearchtextbox"));
+	driver.findElement(By.cssSelector("span.hm-icon-label"));
+	driver.findElement(By.cssSelector("input[id=twotabsearchtextbox]"));
+	driver.findElement(By.cssSelector("a#nav-hamburger-menu[data-csa-c-slot-id=HamburgerMenuDesktop]"));
+	/*tag-id*/
+	driver.findElement(By.cssSelector("span#nav-link-accountList-nav-line-1"));
+	driver.findElement(By.cssSelector("div#nav-cart-count-container"));
+	driver.findElement(By.cssSelector("span#glow-ingress-line1"));
+	driver.findElement(By.cssSelector("a#nav-logo-sprites"));
+	driver.findElement(By.cssSelector("input#nav-search-submit-button"));
+	//tag-class
+	driver.findElement(By.cssSelector("a.nav-imageHref"));
+	driver.navigate().to("https://www.flipkart.com/");
+	driver.findElement(By.cssSelector("div.exehdJ"));
+	driver.findElement(By.cssSelector("input._3704LK"));
+	driver.findElement(By.cssSelector("a._1_3w1N"));
+	driver.navigate().back();
+	driver.navigate().to("https://www.selenium.dev/");
+	driver.findElement(By.cssSelector("a.navbar-brand"));
+	driver.navigate().back();
+	//tag-anyattribute
+	driver.findElement(By.cssSelector("select#searchDropdownBox[name=url]"));
+	driver.findElement(By.cssSelector("div#pageContent[role=main]"));
+	driver.findElement(By.cssSelector("a.nav-imageHref[target=_blank]"));
+	
+	
+	
+	}
+	public void locateByLinkText() {
+	driver.findElement(By.linkText("Sell on Amazon"));
+	driver.findElement(By.linkText("Amazon miniTV"));
+	driver.findElement(By.linkText("Mobiles"));
+	driver.navigate().to("https://www.flipkart.com/");
+	driver.findElement(By.linkText("Login"));
+	driver.navigate().back();
+	driver.findElement(By.linkText("Sell"));
+	driver.navigate().to("https://www.selenium.dev/");
+	driver.findElement(By.linkText("@diegofmolina"));
+	driver.navigate().back();
+	}
+	public void loacteByPartialLinkText() {
+	driver.findElement(By.partialLinkText("Today's"));
+	driver.findElement(By.partialLinkText("Best"));
+	driver.findElement(By.partialLinkText("Amazon"));
+    driver.findElement(By.partialLinkText("Care"));
+    driver.findElement(By.partialLinkText("Sign"));
+    driver.findElement(By.partialLinkText("miniTV"));
+	}
 	 public static void main(String args[]) {
 		 Locators locators= new Locators();
 		 locators.intilaizeBrowser();
-		 locators.locateById();
-		 locators.locateByClassName();
-		 locators.locateByName();
+		 //locators.locateById();
+		// locators.locateByClassName();
+		 //locators.locateByName();
+		 locators.locateByCssSelector();
+		// locators.locateByLinkText();
+		 //locators.loacteByPartialLinkText();
 		 locators.driverQuit();
 	 }
 }
